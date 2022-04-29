8ecd24853cc7611a11e1b0477bcc50017fe50e90
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-04-29 10:01:12 +0700

    add loading screen

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:brew/services/auth.dart';
 import 'package:brew/shared/constants.dart';
+import 'package:brew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -13,69 +14,84 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _authService = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: const Text('Sign Up to Brew Crew'),
-        actions: [
-          TextButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: const Icon(Icons.person),
-              label: const Text('Sign In'))
-        ],
-      ),
-      body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    onChanged: ((value) {
-                      setState(() {
-                        email = value;
-                      });
-                    }),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                    obscureText: true,
-                    onChanged: ((value) {
-                      setState(() {
-                        password = value;
-                      });
-                    }),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await _authService.registerWithEmailAndPassword(
-                          email, password);
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: const Text('Sign Up to Brew Crew'),
+              actions: [
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                    child: const Text('Sign Up'),
-                  )
-                ],
-              ))),
-    );
+                    icon: const Icon(Icons.person),
+                    label: const Text('Sign In'))
+              ],
+            ),
+            body: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'Email'),
+                          onChanged: ((value) {
+                            setState(() {
+                              email = value;
+                            });
+                          }),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Password'),
+                          obscureText: true,
+                          onChanged: ((value) {
+                            setState(() {
+                              password = value;
+                            });
+                          }),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              loading = true;
+                            });
+                            dynamic result = await _authService
+                                .registerWithEmailAndPassword(email, password);
+
+                            if (result == null) {
+                              setState(() {
+                                loading = false;
+                              });
+                            }
+                          },
+                          child: const Text('Sign Up'),
+                        )
+                      ],
+                    ))),
+          );
   }
 }
 
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew/services/auth.dart';
 import 'package:brew/shared/constants.dart';
+import 'package:brew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -12,67 +13,81 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
+  bool loading = false;
 
   String email = '';
   String password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: const Text('Sign In to Brew Crew'),
-        actions: [
-          TextButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: const Icon(Icons.person),
-              label: const Text('Register'))
-        ],
-      ),
-      body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-          child: Form(
-              child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                onChanged: ((value) {
-                  setState(() {
-                    email = value;
-                  });
-                }),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                obscureText: true,
-                onChanged: ((value) {
-                  setState(() {
-                    password = value;
-                  });
-                }),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await _authService.signInWithEmailAndPassword(
-                      email, password);
-                },
-                child: const Text('Sign In'),
-              )
-            ],
-          ))),
-    );
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: const Text('Sign In to Brew Crew'),
+              actions: [
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: const Icon(Icons.person),
+                    label: const Text('Register'))
+              ],
+            ),
+            body: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                child: Form(
+                    child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      onChanged: ((value) {
+                        setState(() {
+                          email = value;
+                        });
+                      }),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Password'),
+                      obscureText: true,
+                      onChanged: ((value) {
+                        setState(() {
+                          password = value;
+                        });
+                      }),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          loading = true;
+                        });
+                        dynamic result = await _authService
+                            .signInWithEmailAndPassword(email, password);
+                        if (result == null) {
+                          setState(() {
+                            loading = false;
+                          });
+                        }
+                      },
+                      child: const Text('Sign In'),
+                    )
+                  ],
+                ))),
+          );
   }
 }
